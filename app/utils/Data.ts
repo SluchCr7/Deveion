7da90a711d427bd01719634185f3a2954d7b5bb6
Sluch <ahmedking10710@gmail.com>
2025-11-17 09:53:00 +0200

    Initial commit from Create Next App

---
+++ app/utils/Data.ts
@@ -0,0 +1,11 @@
+import { FaFacebook,FaInstagram,FaYoutube,FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+
+export const socialIcons = [
+    { icon: <FaFacebook />, link: "/" },
+    { icon: <FaInstagram />, link: "/" },
+    { icon: <FaYoutube />, link: "/" },
+    { icon: <FaLinkedin />, link: "/" },
+    { icon: <FaXTwitter />, link: "/" },
+]
